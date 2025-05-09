143e960a9bca57d5af8dee04cf6784d54f3d9964
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-09 18:07:04 -0300

    Update ExchangeRate.java

--- src/ExchangeRate.java
+++ src/ExchangeRate.java
@@ -1,12 +1,23 @@
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class ExchangeRate {
 
     public rate (){
 
+
+        URI urlCreate = URI.create("https://v6.exchangerate-api.com/v6/8a494251f5c4cf938268006a/latest/USD")
+
         HttpClient client = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NORMAL)
                 .build();
+
+         HttpRequest request = HttpRequest.newBuilder()
+                .uri(urlCreate)
+                .build();
+
+
 
 
     }
