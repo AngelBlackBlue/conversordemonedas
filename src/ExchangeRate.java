241f5def34c0048b42106fcc4350c998ff2a8938
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-13 00:17:44 -0300

    update

--- src/ExchangeRate.java
+++ src/ExchangeRate.java
@@ -1,32 +1,37 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-public class ExchangeRate {
-
-    public rate (){
-
-
-        URI urlCreate = URI.create("https://v6.exchangerate-api.com/v6/8a494251f5c4cf938268006a/pair/USD/ARS");
-
-        HttpClient client = HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .build();
-
-         HttpRequest request = HttpRequest.newBuilder()
-                .uri(urlCreate)
-                .build();
-
-        try {
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
-}
+//import com.google.gson.Gson;
+//
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//
+//public class ExchangeRate {
+//
+//    public rate (){
+//
+//
+//        URI urlCreate = URI.create("https://v6.exchangerate-api.com/v6/8a494251f5c4cf938268006a/pair/USD/ARS");
+//
+//        HttpClient client = HttpClient.newBuilder()
+//                .followRedirects(HttpClient.Redirect.NORMAL)
+//                .build();
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(urlCreate)
+//                .build();
+//
+//        try {
+//            HttpResponse<String> response = client
+//                    .send(request, HttpResponse.BodyHandlers.ofString());
+//
+//            return new Gson().fromJson(response.body(), ConversionRate.class);
+//
+//        } catch (IOException | InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//    }
+//
+//}
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,71 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+
+        String moneda1;
+        String moneda2;
+
+        Scanner lectura = new Scanner(System.in);
+
+
+        while (true){
+
+            System.out.println("""
+                **********************************************************
+                Sea bienvenido/a al Conversor de moneda
+                
+                1) Dólar ==> Peso Argentino
+                2) Peso Argentino ==> Dólar
+                3) Dólar ==> Real Brasileño
+                4) Real Brasileño ==> Dólar
+                5) Dólar => Peso Colombiano
+                6) Peso Colombiano => Dólar
+                7) Salir
+                Elija una opción válidad:
+                **********************************************************
+                """);
+
+            int numero = Integer.valueOf(lectura.nextLine());
+
+            switch (numero) {
+                case 1:
+                    System.out.println("Lunes");
+                    moneda1 = "USD";
+                    moneda2 = "ARS";
+                    break;
+                case 2:
+                    System.out.println("Martes");
+                    moneda1 = "ARS";
+                    moneda2 = "USD";
+                    break;
+                case 3:
+                    System.out.println("Miércoles");
+                    break;
+                case 4:
+                    System.out.println("Jueves");
+                    break;
+                case 5:
+                    System.out.println("Viernes");
+                    break;
+                case 6:
+                    System.out.println("Sábado");
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Día inválido");
+            }
+
+            if (numero == 7) {
+                System.out.println("Gracias por usar conversor de monedas");
+                break;
+
+            } 
+
+
+        }
+
 
     }
 }
